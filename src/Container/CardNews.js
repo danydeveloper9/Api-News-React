843690aed00a5aa126ed88d987159df3b4dcefef
dany DEV <59677234+danydeveloper9@users.noreply.github.com>
2021-09-25 00:12:11 -0500

    Add files via upload

--- src/Container/CardNews.js
+++ src/Container/CardNews.js
@@ -5,7 +5,7 @@ const CardNews = n => {
     return (
 
         <article className="news-item">
-			<img src={n.imagen} alt="imagen">
+			<img src={n.imagen} alt="imagen" />
 			<section>
 				<h2 className="news-title"> {n.nombre} </h2>
 				<p> {n.fecha.substring(0, 10)} </p>
